406df0aa55ea351b8fb37a53043e44344c40db90
Avcuongy <dangxuancuong123456789@gmail.com>
2026-06-23 16:31:57 +0700

    .env

--- src/utils/config_env.py
+++ src/utils/config_env.py
@@ -3,14 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# API
-SEC_API_KEY = os.getenv("SEC_API_KEY")
-ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
-MASSIVE_API_KEY = os.getenv("MASSIVE_API_KEY")
-
-# Database
-DATABASE_URL = os.getenv("DATABASE_URL")
-
 # Airflow
 AIRFLOW_UID = os.getenv("AIRFLOW_UID", "50000")
 AIRFLOW_FERNET_KEY = os.getenv("AIRFLOW_FERNET_KEY")
